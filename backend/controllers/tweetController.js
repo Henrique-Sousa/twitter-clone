4cb027e34576bc94a14c16cbd78beabe82654097
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-08-02 20:15:16 -0300

    refactor(backend): add try/catch to crud functions

--- backend/controllers/tweetController.js
+++ backend/controllers/tweetController.js
@@ -1,36 +1,58 @@
 const Tweet = require('../models/tweet');
+const createError = require('http-errors');
 
-exports.index = async (req, res) => {  
-	const results = await Tweet.findAll();
-	res.send(results);
+exports.index = async (req, res, next) => {  
+	try {
+		const results = await Tweet.findAll();
+		res.send(results);
+	} catch (e) {
+		next(createError(500));
+	}
+	
 }
 
-exports.create = async (req, res) => {
-	const tweet = await Tweet.create({
-		author: req.body.author,
-		date: Date.now(),
-		text: req.body.text
-	});
-	res.send(tweet);
+exports.create = async (req, res, next) => {
+	try {
+		const tweet = await Tweet.create({
+			author: req.body.author,
+			date: Date.now(),
+			text: req.body.text
+		});
+		res.send(tweet);
+	} catch (e) {
+		next(createError(500));
+	}
 }
 
-exports.show = async  (req, res) => {
-	const result = await Tweet.findByPk(req.params.tweet_id);
-	res.send(result);
+exports.show = async (req, res) => {
+	try {
+		const result = await Tweet.findByPk(req.params.tweet_id);
+		res.send(result);
+	} catch (e) {
+		next(createError(500));
+	}
 }
 
 exports.update = async (req, res) => {
-	const result = await Tweet.update(
-		{ text: req.body.text },
-		{ where: { id: req.params.tweet_id } }
-	);
-	res.send(result);
+	try {
+		const result = await Tweet.update(
+			{ text: req.body.text },
+			{ where: { id: req.params.tweet_id } }
+		);
+		res.send(result);
+	} catch (e) {
+		next(createError(500));
+	}
 }
 
 exports.destroy = async (req, res) => {
-	await Tweet.destroy({
-		where: { id: req.params.tweet_id } 
-	});
-	res.end();
+	try {
+		await Tweet.destroy({
+			where: { id: req.params.tweet_id } 
+		});
+		res.end();
+	} catch (e) {
+		next(createError(500));
+	}
 }
 
