a9b898c957bb3a86718b547f2a1f9355fea542ad
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-08-13 23:34:01 -0300

    style(frontend): add new lines to component attributes

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -45,9 +45,14 @@ export default class Feed extends Component {
       <div className="feed">
         <div><h1>Home</h1></div>
         <WhatsHappening apiURL={this.props.apiURL} />
-        {this.state.tweets.map((tweet) =>
-          <Tweet key={tweet.id} authorName={tweet.authorName} authorNickName={tweet.authorNickName} text={tweet.text} />
-        )}
+        {this.state.tweets.map((tweet) => (
+          <Tweet
+            key={tweet.id}
+            authorName={tweet.authorName}
+            authorNickName={tweet.authorNickName}
+            text={tweet.text}
+          />
+        ))}
       </div>
     );
   }
