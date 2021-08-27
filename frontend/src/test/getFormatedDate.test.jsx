63fcffc927faaa54e7d95b9db417e26a57da8a27
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-08-27 00:40:07 -0300

    test(frontend): add time variables; add hours

--- frontend/src/test/getFormatedDate.test.jsx
+++ frontend/src/test/getFormatedDate.test.jsx
@@ -4,10 +4,13 @@ describe('test formated date', () => {
   const baseTime = 1600 * (10 ** 9);
   const baseDate = new Date(baseTime);
   const getDateBefore = (time) => new Date(baseTime - time);
+  const s = 1000;
+  const m = 60 * s;
+  const h = 60 * m;
 
   test('seconds', () => {
     expect(getFormatedDate(
-      getDateBefore(30 * 1000),
+      getDateBefore(30 * s),
       baseDate,
     ))
       .toBe('30s');
@@ -15,9 +18,17 @@ describe('test formated date', () => {
 
   test('minutes', () => {
     expect(getFormatedDate(
-      getDateBefore(70 * 1000),
+      getDateBefore(m + 10 * s),
       baseDate,
     ))
       .toBe('1m');
+  });
+
+  test('hours', () => {
+    expect(getFormatedDate(
+      getDateBefore(2 * h + 21 * m + 35 * s),
+      baseDate,
+    ))
+      .toBe('2h');
   });
 });
