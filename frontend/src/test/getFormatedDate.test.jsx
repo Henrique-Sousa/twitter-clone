12df85623a87a1bb66517c698cb45b9d99f8ed22
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-08-27 23:16:09 -0300

    test(frontend): test past years
      Test tweets one or more years old
      Replace 1995 with 2021

--- frontend/src/test/getFormatedDate.test.jsx
+++ frontend/src/test/getFormatedDate.test.jsx
@@ -1,12 +1,13 @@
 import getFormatedDate from '../lib/getFormatedDate';
 
 describe('test formated date', () => {
-  const baseDate = new Date('December 17, 1995 03:24:00');
-  const thirtySecsBefore = new Date('December 17, 1995 03:23:30');
-  const oneMinBefore = new Date('December 17, 1995 03:23:00');
-  const twoHoursBefore = new Date('December 17, 1995 01:19:15');
-  const dec14 = new Date('December 14, 1995 17:33:43');
-  const feb21 = new Date('February 21, 1995 13:23:21');
+  const baseDate = new Date('December 17, 2021 03:24:00');
+  const thirtySecsBefore = new Date('December 17, 2021 03:23:30');
+  const oneMinBefore = new Date('December 17, 2021 03:23:00');
+  const twoHoursBefore = new Date('December 17, 2021 01:19:15');
+  const dec14 = new Date('December 14, 2021 17:33:43');
+  const feb21 = new Date('February 21, 2021 13:23:21');
+  const may17Year2020 = new Date('May 17, 2020 13:23:21');
 
   test('seconds', () => {
     expect(getFormatedDate(
@@ -46,5 +47,13 @@ describe('test formated date', () => {
       baseDate,
     ))
       .toBe('Feb 21');
+  });
+
+  test('May 17, 2020', () => {
+    expect(getFormatedDate(
+      may17Year2020,
+      baseDate,
+    ))
+      .toBe('May 17, 2020');
   });
 });
