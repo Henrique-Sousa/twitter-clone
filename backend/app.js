4a1a80dfcd5617bf556052aa51ca6fd74328bd07
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-09-18 23:57:35 -0300

    fix(backend error handling): add `next` argument

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   const error = req.app.get('env') === 'development' ? err : '';
   res.status(error.status || 500);
   res.end();
