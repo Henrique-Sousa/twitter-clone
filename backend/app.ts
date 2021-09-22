cc93fedcdba1b957a80cd3a7cd5a820b22c95456
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-09-22 10:42:31 -0300

    feat(orm): remove sequelize connection and add typeorm connection

---
+++ backend/app.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+import { createConnection, ConnectionOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+import User from './src/entity/User';
+
+const main = async () => {
+  dotenv.config();
+  const options: ConnectionOptions = {
+    type: 'postgres',
+    port: 5432,
+    host: process.env.NODE_ENV === 'production' ? process.env.POSTGRES_HOST : 'localhost',
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: true,
+    logging: false,
+    entities: [User],
+  };
+  const connection = await createConnection(options);
+};
+
+main().catch((error) => console.error(error));
