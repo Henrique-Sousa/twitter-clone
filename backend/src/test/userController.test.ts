75fac47fe83e68266bad3568ae8f0a93e2f34572
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-09-23 15:50:55 -0300

    test: add test to user route/controller

---
+++ backend/src/test/userController.test.ts
@@ -0,0 +1,54 @@
+import 'reflect-metadata';
+import request from 'supertest';
+import express from 'express';
+import {
+  ConnectionOptions,
+  createConnection,
+  getConnection,
+  getRepository,
+} from 'typeorm';
+import User from '../entity/User';
+import Tweet from '../entity/Tweet';
+import users from '../routes/users';
+
+beforeEach(() => {
+  const options: ConnectionOptions = {
+    type: 'sqlite',
+    database: ':memory:',
+    dropSchema: true,
+    entities: [User, Tweet],
+    synchronize: true,
+    logging: false,
+  };
+  return createConnection(options);
+});
+
+afterEach(() => {
+  const conn = getConnection();
+  return conn.close();
+});
+
+const app = express();
+
+app.use('/', users);
+
+test('users route', async () => {
+  await getRepository(User).insert({
+    name: 'Barack Obama',
+    username: 'BarackObama',
+  });
+  await getRepository(User).insert({
+    name: 'Justin Bieber',
+    username: 'justinbieber',
+  });
+  const result = await request(app)
+    .get('/')
+    .expect('Content-Type', /json/)
+    .expect(200);
+  expect(result.body[0].id).toBe(1);
+  expect(result.body[1].id).toBe(2);
+  expect(result.body[0].name).toBe('Barack Obama');
+  expect(result.body[1].name).toBe('Justin Bieber');
+  expect(result.body[0].username).toBe('BarackObama');
+  expect(result.body[1].username).toBe('justinbieber');
+});
