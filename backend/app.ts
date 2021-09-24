0f0a5291d9d2b4b90bd035ff37d541b06fa6655b
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-09-24 12:12:53 -0300

    feat: complete typerscript version of app.js

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,36 @@
 import 'reflect-metadata';
 import express from 'express';
+import createError from 'http-errors';
 import usersRouter from './src/routes/users';
 import tweetsRouter from './src/routes/tweets';
 import './src/database';
 
+type Request = express.Request;
+type Response = express.Response;
+type NextFunction = express.NextFunction;
+type HttpError = createError.HttpError;
+
 const app = express();
+
+app.use(express.json());
+
+app.all('*', (req: Request, res: Response, next: NextFunction): void => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use('/users', usersRouter);
 app.use('/tweets', tweetsRouter);
+
+app.use((req: Request, res: Response, next: NextFunction): void => {
+  next(createError(404));
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction): void => {
+  res.status(err.status || 500);
+  res.end();
+});
 
 app.listen(3000);
