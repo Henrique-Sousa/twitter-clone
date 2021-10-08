003d50ad66e20517a6863a90ea4609668d5f8d1e
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-10-08 16:35:18 -0300

    refactor: move api url to .env file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Sidebar from './components/Sidebar';
 
 const App: FC = () => {
 
-  const apiURL: string = 'http://127.0.0.1:3001';
+  const apiURL: string = process.env.REACT_APP_API_URL || 'http://127.0.0.1:3001';
 
   return (
     <>
